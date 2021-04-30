6304c3d8ada3e32b955e6894223c0abee35d94b1
Katie Baker <katie.m.baker87@gmail.com>
2021-04-30 11:52:59 +0100

    Updated Monster class to add init and str abstract methods

---
+++ dmai/domain/monsters/monster.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+class Monster(ABC):
+    '''Monster abstract class'''
+
+    @abstractmethod
+    def __init__():
+        pass
+    
+    @abstractmethod
+    def __str__():
+        pass
