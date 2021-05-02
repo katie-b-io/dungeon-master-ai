04c2c4a3f14ae2f31e5ffb3b17f7685b8fd894bb
Katie Baker <katie.m.baker87@gmail.com>
2021-05-02 15:23:49 +0100

    Created CharacterCollection class

---
+++ dmai/domain/characters/__init__.py
@@ -0,0 +1,6 @@
+from .character import Character
+from .cleric import Cleric
+from .fighter import Fighter
+from .rogue import Rogue
+from .wizard import Wizard
+from .character_collection import CharacterCollection
