772f1bf7fff804b494c08f6c9c3a1f7ff6d62958
Katie Baker <katie.m.baker87@gmail.com>
2021-05-02 15:23:27 +0100

    Created Wizard class

---
+++ dmai/domain/characters/wizard.py
@@ -0,0 +1,6 @@
+from dmai.domain.characters import Character
+
+class Wizard(Character):
+    
+    def __init__(self, character_data: dict) -> None:
+        Character.__init__(self, character_data)
