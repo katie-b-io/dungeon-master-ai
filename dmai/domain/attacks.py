096c1eb55ec344f3cca9ad2b18157e5fa8d9dd59
Katie Baker <katie.m.baker87@gmail.com>
2021-05-03 20:16:53 +0100

    Changed constructor and used __repr__ instead of __str__ for Attacks

--- dmai/domain/attacks.py
+++ dmai/domain/attacks.py
@@ -5,12 +5,12 @@ class Attacks():
     # class variables
     attack_data = dict()
     
-    def __init__(self, attacks: dict) -> None:
+    def __init__(self, attacks: dict = {}) -> None:
         '''Attacks class'''
         self.attacks = attacks
         self._load_attack_data()
         
-    def __str__(self) -> str:
+    def __repr__(self) -> str:
         return "Attacks:\n{a}".format(a=self.attacks)
     
     @classmethod
