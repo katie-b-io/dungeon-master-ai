50fd6f380b6b9ae869e77ed7d3a21a284d230263
Katie Baker <katie.m.baker87@gmail.com>
2021-05-03 20:11:36 +0100

    Created CharacterClass class

---
+++ dmai/domain/characters/character_class.py
@@ -0,0 +1,37 @@
+from dmai.utils import Loader
+
+class CharacterClass():
+    
+    # class variables
+    char_class_data = dict()
+    
+    def __init__(self, char_class: dict) -> None:
+        '''CharacterClass class'''
+        self._load_char_class_data()
+        
+        try:
+            self.char_class = list(char_class.keys())[0]
+            self.level = char_class[self.char_class]["level"]
+            if char_class[self.char_class]["subclass"]:
+                self.subclass = char_class[self.char_class]["subclass"]
+            else:
+                self.subclass = None
+            
+            for key in self.char_class_data[self.char_class]:
+                self.__setattr__(key, self.char_class_data[self.char_class][key])
+        
+        except KeyError as e:
+            print("Class does not exist: {c}".format(c=e))
+            raise
+        except AttributeError as e:
+            print("Cannot create class, incorrect attribute: {e}".format(e=e))
+            raise
+        
+    def __repr__(self) -> str:
+        return "Class: {a}".format(a=self.name)
+    
+    @classmethod
+    def _load_char_class_data(self) -> None:
+        '''Set the self.char_class_data class variable data'''
+        self.char_class_data = Loader.load_json("data/classes.json")
+    
