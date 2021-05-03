21b430dd973f3cda5fee353629031d0f8d59d2ca
Katie Baker <katie.m.baker87@gmail.com>
2021-05-03 20:10:46 +0100

    Updated Features class

--- dmai/domain/features.py
+++ dmai/domain/features.py
@@ -5,16 +5,25 @@ class Features():
     # class variables
     feature_data = dict()
     
-    def __init__(self, features: dict) -> None:
+    def __init__(self, char_class = None, race = None, features: list = None) -> None:
         '''Features class'''
-        self.features = features
         self._load_feature_data()
+        self.features = list()
         
-    def __str__(self) -> str:
+        if features:
+            self.features.extend(features)
+        
+        if char_class:
+            self.features.extend(char_class.features)
+        
+        if race:
+            self.features.extend(race.traits)
+        
+    def __repr__(self) -> str:
         return "Features:\n{a}".format(a=self.features)
     
     @classmethod
     def _load_feature_data(self) -> None:
         '''Set the self.feature_data class variable data'''
         self.feature_data = Loader.load_json("data/features.json")
-        
+        self.feature_data.update(Loader.load_json("data/monster_features.json"))
