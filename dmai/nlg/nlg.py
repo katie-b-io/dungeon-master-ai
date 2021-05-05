d45915b4d4bf3cb7ff2b30a0531e06d5b634db99
Katie Baker <katie.m.baker87@gmail.com>
2021-05-05 14:27:40 +0100

    Refactored NLG into Singleton

--- dmai/nlg/nlg.py
+++ dmai/nlg/nlg.py
@@ -1,8 +1,23 @@
 import random
 
-class NLG():
+class NLGMeta(type):
+    _instances = {}
     
+    def __call__(cls, *args, **kwargs) -> None:
+        '''NLG static singleton metaclass'''
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+class NLG(metaclass=NLGMeta):
+    
+    # class variable
     game = None
+    
+    def __init__(self) -> None:
+        '''NLG static class'''
+        pass
     
     @classmethod
     def set_game(cls, game) -> None:
@@ -11,7 +26,7 @@ class NLG():
     @classmethod
     def get_char_class(cls) -> str:
         '''Return utterance for character selection'''
-        c = "\n".join(cls.game.domain.characters.get_all_names())
+        c = "\n".join(cls.game.dm.domain.characters.get_all_names())
         utters = [
             "Which character class would you like to play?\n{c}".format(c=c),
             "Select a character class you like the sound of:\n{c}".format(c=c),
@@ -42,7 +57,7 @@ class NLG():
     @classmethod
     def get_title(cls) -> str:
         '''Return the utterance for introducing the adventure title'''
-        t = cls.game.adventure.title
+        t = cls.game.dm.adventure.title
         utters = [
             "Welcome adventurer, today we're going to play {t}! Let me set the scene...".format(t=t),
             "Today we'll play {t}, an exciting tale of adventure! Let me set the scene...".format(t=t),
