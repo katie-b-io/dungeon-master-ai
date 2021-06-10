9589a637d5f30299a7210515c44c801444c64bba
Katie Baker <katie.m.baker87@gmail.com>
2021-06-10 19:57:49 +0000

    Implemented more problem building code

--- dmai/planning/planning_agent.py
+++ dmai/planning/planning_agent.py
@@ -55,3 +55,42 @@ class PlanningAgent(ABC):
     def get_next_move(self) -> str:
         plan = self.planner.parse_plan()
         return plan[0]
+
+    def _construct_problem_header(self, problem: str, domain: str) -> str:
+        return "(define (problem {p}) (:domain {d})\n".format(p=problem, d=domain)
+    
+    def _construct_problem_footer(self) -> str:
+        return ")\n"
+
+    def _construct_objects(self, objects: list) -> str:
+        """Method expects a list of tuples of one or two length.
+        If length is two, the tuple is (object, type)"""
+        objects_str = "(:objects\n"
+        for obj in objects:
+            if len(obj) == 1:
+                objects_str += "{o}\n".format(o=obj[0])
+            else:
+                objects_str += "{o} - {t}\n".format(o=obj[0], t=obj[1])
+        objects_str += ")\n"
+        return objects_str
+    
+    def _construct_init(self, init: list) -> str:
+        """Method expects a list of tuples of any length.
+        Tuples will be constructed into PDDL strings, e.g. (one, two, three)
+        becomes (one two three)"""
+        init_str = "(:init\n"
+        for obj in init:
+            init_str += "({s})\n".format(s=" ".join(obj))
+        init_str += ")\n"
+        return init_str
+    
+    def _construct_goal(self, goal: list) -> str:
+        """Method expects a list of tuples of any length.
+        Tuples will be constructed into PDDL strings, e.g. (one, two, three)
+        becomes (one two three)"""
+        goal_str = "(:goal (and\n"
+        for obj in goal:
+            goal_str += "({g})\n".format(g=" ".join(obj))
+        goal_str += "))\n"
+        return goal_str
+    
