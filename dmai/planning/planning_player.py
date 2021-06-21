4550d3a39b0a5ac290be65ec37cf64fce8eb5ea7
Katie Baker <katie.m.baker87@gmail.com>
2021-06-21 12:47:11 +0000

    Linted planning files

--- dmai/planning/planning_player.py
+++ dmai/planning/planning_player.py
@@ -24,13 +24,14 @@ class PlanningPlayer(PlanningAgent):
         logger.debug("Building domain")
         domain_file = os.path.join(
             Config.directory.planning,
-            "{u}.{d}.domain.pddl".format(u=Config.uuid, d=self.domain))
+            "{u}.{d}.domain.pddl".format(u=Config.uuid, d=self.domain),
+        )
 
         # TODO implement intent solution
         # TODO implement item solution
         # TODO implement solve intent
 
-        with open(domain_file, 'w') as writer:
+        with open(domain_file, "w") as writer:
             ################################################
             # Construct the domain file header and requirements
             writer.write(self._construct_domain_header(self.domain))
@@ -41,28 +42,35 @@ class PlanningPlayer(PlanningAgent):
             types = []
 
             # Append types
-            types.append({
-                "type":
-                "object",
-                "subtypes": [
-                    "entity", "intent", "item", "puzzle", "attitude",
-                    "ability", "room", "door", "weapon", "armor", "equipment",
-                    "damage_vulnerability", "damage_immunity",
-                    "condition_immunity", "language"
-                ]
-            })
-            types.append({
-                "type": "entity",
-                "subtypes": ["player", "npc", "monster"]
-            })
-            types.append({
-                "type": "monster",
-                "subtypes": MonsterCollection.get_all_monsters()
-            })
-            types.append({
-                "type": "attitude",
-                "subtypes": ["indifferent", "friendly", "hostile"]
-            })
+            types.append(
+                {
+                    "type": "object",
+                    "subtypes": [
+                        "entity",
+                        "intent",
+                        "item",
+                        "puzzle",
+                        "attitude",
+                        "ability",
+                        "room",
+                        "door",
+                        "weapon",
+                        "armor",
+                        "equipment",
+                        "damage_vulnerability",
+                        "damage_immunity",
+                        "condition_immunity",
+                        "language",
+                    ],
+                }
+            )
+            types.append({"type": "entity", "subtypes": ["player", "npc", "monster"]})
+            types.append(
+                {"type": "monster", "subtypes": MonsterCollection.get_all_monsters()}
+            )
+            types.append(
+                {"type": "attitude", "subtypes": ["indifferent", "friendly", "hostile"]}
+            )
             types.append({"type": "ability", "subtypes": ["skill"]})
             types.append({"type": "weapon", "subtypes": ["ranged_weapon"]})
             writer.write(self._construct_types(types))
@@ -74,132 +82,166 @@ class PlanningPlayer(PlanningAgent):
             # Adventure
             predicates.append({"predicate": "quest", "params": []})
             predicates.append({"predicate": "complete", "params": []})
-            predicates.append({
-                "predicate": "gives_quest",
-                "params": [("npc", "npc")]
-            })
-            predicates.append({
-                "predicate": "advantage",
-                "params": [("object", "object")]
-            })
-            predicates.append({
-                "predicate": "disadvantage",
-                "params": [("object", "object")]
-            })
+            predicates.append({"predicate": "gives_quest", "params": [("npc", "npc")]})
+            predicates.append(
+                {"predicate": "advantage", "params": [("object", "object")]}
+            )
+            predicates.append(
+                {"predicate": "disadvantage", "params": [("object", "object")]}
+            )
             for ability in Abilities.get_all_abilities():
-                predicates.append({
-                    "predicate": ability[1].lower(),
-                    "params": [("ability", "ability")]
-                })
+                predicates.append(
+                    {
+                        "predicate": ability[1].lower(),
+                        "params": [("ability", "ability")],
+                    }
+                )
             for skill in Skills.get_all_skills():
-                predicates.append({
-                    "predicate": skill[0],
-                    "params": [("skill", "skill")]
-                })
-            predicates.append({
-                "predicate": "has",
-                "params": [("entity", "entity"), ("object", "object")]
-            })
-            predicates.append({
-                "predicate": "at",
-                "params": [("object", "object"), ("room", "room")]
-            })
-            predicates.append({
-                "predicate": "alive",
-                "params": [("object", "object")]
-            })
-            predicates.append({
-                "predicate": "damaged",
-                "params": [("object", "object")]
-            })
-            predicates.append({
-                "predicate": "equipped",
-                "params": [("player", "player"), ("object", "object")]
-            })
-            predicates.append({
-                "predicate": "connected",
-                "params": [("door", "door"), ("location", "room"),
-                           ("destination", "room")]
-            })
-            predicates.append({
-                "predicate": "locked",
-                "params": [("door", "door")]
-            })
-            predicates.append({
-                "predicate": "higher_than_ac",
-                "params": [("target", "object")]
-            })
-            predicates.append({
-                "predicate": "can_attack_roll",
-                "params": [("player", "player"), ("target", "object")]
-            })
-            predicates.append({
-                "predicate": "can_damage_roll",
-                "params": [("player", "player"), ("target", "object")]
-            })
-            predicates.append({
-                "predicate": "can_ability_check",
-                "params": [("player", "player"), ("ability", "ability"),
-                           ("target", "object")]
-            })
-            predicates.append({
-                "predicate": "can_equipment_check",
-                "params": [("player", "player"), ("equipment", "equipment"),
-                           ("target", "object")]
-            })
-            predicates.append({
-                "predicate": "ability_check_success",
-                "params": [("player", "player"), ("ability", "ability"),
-                           ("target", "object")]
-            })
-            predicates.append({
-                "predicate": "equipment_check_success",
-                "params": [("player", "player"), ("equipment", "equipment"),
-                           ("target", "object")]
-            })
-            predicates.append({
-                "predicate": "attack_roll_success",
-                "params": [("player", "player"), ("target", "object")]
-            })
+                predicates.append(
+                    {"predicate": skill[0], "params": [("skill", "skill")]}
+                )
+            predicates.append(
+                {
+                    "predicate": "has",
+                    "params": [("entity", "entity"), ("object", "object")],
+                }
+            )
+            predicates.append(
+                {"predicate": "at", "params": [("object", "object"), ("room", "room")]}
+            )
+            predicates.append({"predicate": "alive", "params": [("object", "object")]})
+            predicates.append(
+                {"predicate": "damaged", "params": [("object", "object")]}
+            )
+            predicates.append(
+                {
+                    "predicate": "equipped",
+                    "params": [("player", "player"), ("object", "object")],
+                }
+            )
+            predicates.append(
+                {
+                    "predicate": "connected",
+                    "params": [
+                        ("door", "door"),
+                        ("location", "room"),
+                        ("destination", "room"),
+                    ],
+                }
+            )
+            predicates.append({"predicate": "locked", "params": [("door", "door")]})
+            predicates.append(
+                {"predicate": "higher_than_ac", "params": [("target", "object")]}
+            )
+            predicates.append(
+                {
+                    "predicate": "can_attack_roll",
+                    "params": [("player", "player"), ("target", "object")],
+                }
+            )
+            predicates.append(
+                {
+                    "predicate": "can_damage_roll",
+                    "params": [("player", "player"), ("target", "object")],
+                }
+            )
+            predicates.append(
+                {
+                    "predicate": "can_ability_check",
+                    "params": [
+                        ("player", "player"),
+                        ("ability", "ability"),
+                        ("target", "object"),
+                    ],
+                }
+            )
+            predicates.append(
+                {
+                    "predicate": "can_equipment_check",
+                    "params": [
+                        ("player", "player"),
+                        ("equipment", "equipment"),
+                        ("target", "object"),
+                    ],
+                }
+            )
+            predicates.append(
+                {
+                    "predicate": "ability_check_success",
+                    "params": [
+                        ("player", "player"),
+                        ("ability", "ability"),
+                        ("target", "object"),
+                    ],
+                }
+            )
+            predicates.append(
+                {
+                    "predicate": "equipment_check_success",
+                    "params": [
+                        ("player", "player"),
+                        ("equipment", "equipment"),
+                        ("target", "object"),
+                    ],
+                }
+            )
+            predicates.append(
+                {
+                    "predicate": "attack_roll_success",
+                    "params": [("player", "player"), ("target", "object")],
+                }
+            )
             for equipment in State.get_player().get_all_equipment_ids():
-                predicates.append({
-                    "predicate": equipment,
-                    "params": [("equipment", "equipment")]
-                })
+                predicates.append(
+                    {"predicate": equipment, "params": [("equipment", "equipment")]}
+                )
             predicates.append({"predicate": "action", "params": []})
-            predicates.append({
-                "predicate": "attitude_towards_player",
-                "params": [("npc", "npc"), ("attitude", "attitude")]
-            })
-            predicates.append({
-                "predicate": "improve_attitude",
-                "params": [("current", "attitude"), ("next", "attitude")]
-            })
-            predicates.append({
-                "predicate": "degrade_attitude",
-                "params": [("current", "attitude"), ("next", "attitude")]
-            })
+            predicates.append(
+                {
+                    "predicate": "attitude_towards_player",
+                    "params": [("npc", "npc"), ("attitude", "attitude")],
+                }
+            )
+            predicates.append(
+                {
+                    "predicate": "improve_attitude",
+                    "params": [("current", "attitude"), ("next", "attitude")],
+                }
+            )
+            predicates.append(
+                {
+                    "predicate": "degrade_attitude",
+                    "params": [("current", "attitude"), ("next", "attitude")],
+                }
+            )
             predicates.append({"predicate": "combat", "params": []})
-            predicates.append({
-                "predicate": "must_kill",
-                "params": [("monster", "monster")]
-            })
-            predicates.append({
-                "predicate": "ability_solution",
-                "params": [("target", "object"), ("ability", "ability")]
-            })
-            predicates.append({
-                "predicate": "equipment_solution",
-                "params": [("target", "object"), ("equipment", "equipment")]
-            })
-            predicates.append({
-                "predicate": "intent_solution",
-                "params": [("target", "object"), ("intent", "intent")]
-            })
-            predicates.append({
-                "predicate": "item_solution",
-                "params": [("target", "object"), ("item", "item")]
-            })
+            predicates.append(
+                {"predicate": "must_kill", "params": [("monster", "monster")]}
+            )
+            predicates.append(
+                {
+                    "predicate": "ability_solution",
+                    "params": [("target", "object"), ("ability", "ability")],
+                }
+            )
+            predicates.append(
+                {
+                    "predicate": "equipment_solution",
+                    "params": [("target", "object"), ("equipment", "equipment")],
+                }
+            )
+            predicates.append(
+                {
+                    "predicate": "intent_solution",
+                    "params": [("target", "object"), ("intent", "intent")],
+                }
+            )
+            predicates.append(
+                {
+                    "predicate": "item_solution",
+                    "params": [("target", "object"), ("item", "item")],
+                }
+            )
             writer.write(self._construct_predicates(predicates))
 
             ################################################
@@ -230,7 +272,7 @@ class PlanningPlayer(PlanningAgent):
                 "roleplay_negatively",
                 "declare_attack_against_entity",
                 "attack_monster",
-                "kill_monster"
+                "kill_monster",
             ]
             if State.get_player().has_equipment("thieves_tools")[0]:
                 actions.append("use_thieves_tools")
@@ -241,13 +283,13 @@ class PlanningPlayer(PlanningAgent):
         logger.debug("Building problem")
         problem_file = os.path.join(
             Config.directory.planning,
-            "{u}.{p}.problem.pddl".format(u=Config.uuid, p=self.problem))
+            "{u}.{p}.problem.pddl".format(u=Config.uuid, p=self.problem),
+        )
 
-        with open(problem_file, 'w') as writer:
+        with open(problem_file, "w") as writer:
             ################################################
             # Construct the problem file header
-            writer.write(self._construct_problem_header(
-                self.problem, "player"))
+            writer.write(self._construct_problem_header(self.problem, "player"))
 
             ################################################
             # Construct the problem file objects
@@ -347,12 +389,23 @@ class PlanningPlayer(PlanningAgent):
             init.append(["degrade_attitude", "friendly", "indifferent"])
             init.append(["degrade_attitude", "indifferent", "hostile"])
             for npc in State.get_dm().npcs.get_all_npcs():
-                init.append(["attitude_towards_player", npc.id, State.get_current_attitude(npc.id).value])
+                init.append(
+                    [
+                        "attitude_towards_player",
+                        npc.id,
+                        State.get_current_attitude(npc.id).value,
+                    ]
+                )
 
             # Monsters
             for monster in State.get_dm().npcs.get_all_monsters():
-                init.append(["at", monster.unique_id,
-                             State.get_current_room(monster.unique_id).id])
+                init.append(
+                    [
+                        "at",
+                        monster.unique_id,
+                        State.get_current_room(monster.unique_id).id,
+                    ]
+                )
             for monster in State.get_dm().npcs.get_all_monsters():
                 if State.is_alive(monster.unique_id):
                     init.append(["alive", monster.unique_id])
@@ -390,8 +443,7 @@ class PlanningPlayer(PlanningAgent):
                         if puzzle.check_solution_skill(skill[0]):
                             init.append(["ability_solution", puzzle.id, skill[0]])
                     # Equipment solution
-                    for equipment in State.get_player().get_all_equipment_ids(
-                    ):
+                    for equipment in State.get_player().get_all_equipment_ids():
                         if puzzle.check_solution_equipment(equipment):
                             init.append(["equipment_solution", puzzle.id, equipment])
                     # Intent solution
