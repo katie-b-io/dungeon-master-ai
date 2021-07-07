bbe9c992d2bf25a5812dfa7c688e9f88327cf8c7
Katie Baker <katie.m.baker87@gmail.com>
2021-07-07 11:29:00 +0100

    Make sure intro works correctly in API

--- dmai/game/game.py
+++ dmai/game/game.py
@@ -98,11 +98,8 @@ class Game:
         elif not self.player.name:
             # player is entering a name
             self.player.set_name(player_utter)
+            State.start()
             succeed = self.dm.input(player_utter, utter_type="name")
-
-        elif not State.started:
-            # start the game by relaying description of starting room
-            succeed = self.dm.input(player_utter, utter_type="start")
 
         elif player_utter:
             # attempt to determine the player's intent
@@ -124,6 +121,10 @@ class Game:
     def output(self) -> str:
         """Return an output for the player"""
 
+        # print welcome text
+        if OutputBuilder.has_response():
+                return OutputBuilder.format()
+        
         # the game starts
         if self.intro:
             try:
