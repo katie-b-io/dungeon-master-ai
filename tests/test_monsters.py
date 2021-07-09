d6021cf4917cb57505a9d3848a30a3b23551d05f
Katie Baker <katie.m.baker87@gmail.com>
2021-07-09 15:47:19 +0000

    Updated test_monsters

--- tests/test_monsters.py
+++ tests/test_monsters.py
@@ -1,3 +1,5 @@
+from dmai.game.state import State
+from dmai.utils.output_builder import OutputBuilder
 import unittest
 import sys
 import os
@@ -19,40 +21,44 @@ class TestMonsterCollection(unittest.TestCase):
     def setUp(self) -> None:
         self.monsters = MonsterCollection()
         self.monsters.load()
+        self.output_builder = OutputBuilder()
+        self.state = State(self.output_builder)
 
     def test_monster_factory_error(self) -> None:
         with self.assertRaises(ValueError):
-            self.monsters._monster_factory("human")
+            self.monsters._monster_factory("human", self.state, self.output_builder)
 
     def test_get_monster_cat(self) -> None:
-        self.assertIsInstance(self.monsters.get_monster("cat", "cat_1"), Cat)
-        self.assertIsInstance(self.monsters.get_monster("CAT", "cat_1"), Cat)
+        self.assertIsInstance(self.monsters.get_monster("cat", self.state, self.output_builder, "cat_1"), Cat)
+        self.assertIsInstance(self.monsters.get_monster("CAT", self.state, self.output_builder, "cat_1"), Cat)
 
     def test_get_monster_giant_rat(self) -> None:
-        self.assertIsInstance(self.monsters.get_monster("giant_rat", "giant_rat_1"), GiantRat)
-        self.assertIsInstance(self.monsters.get_monster("GIANT_RAT", "giant_rat_1"), GiantRat)
+        self.assertIsInstance(self.monsters.get_monster("giant_rat", self.state, self.output_builder, "giant_rat_1"), GiantRat)
+        self.assertIsInstance(self.monsters.get_monster("GIANT_RAT", self.state, self.output_builder, "giant_rat_1"), GiantRat)
 
     def test_get_monster_goblin(self) -> None:
-        self.assertIsInstance(self.monsters.get_monster("goblin", "goblin_1"), Goblin)
-        self.assertIsInstance(self.monsters.get_monster("GOBLIN", "goblin_1"), Goblin)
+        self.assertIsInstance(self.monsters.get_monster("goblin", self.state, self.output_builder, "goblin_1"), Goblin)
+        self.assertIsInstance(self.monsters.get_monster("GOBLIN", self.state, self.output_builder, "goblin_1"), Goblin)
 
     def test_get_monster_skeleton(self) -> None:
-        self.assertIsInstance(self.monsters.get_monster("skeleton", "skeleton_1"), Skeleton)
-        self.assertIsInstance(self.monsters.get_monster("SKELETON", "skeleton_1"), Skeleton)
+        self.assertIsInstance(self.monsters.get_monster("skeleton", self.state, self.output_builder, "skeleton_1"), Skeleton)
+        self.assertIsInstance(self.monsters.get_monster("SKELETON", self.state, self.output_builder, "skeleton_1"), Skeleton)
 
     def test_get_monster_zombie(self) -> None:
-        self.assertIsInstance(self.monsters.get_monster("zombie", "zombie_1"), Zombie)
-        self.assertIsInstance(self.monsters.get_monster("ZOMBIE", "zombie_1"), Zombie)
+        self.assertIsInstance(self.monsters.get_monster("zombie", self.state, self.output_builder, "zombie_1"), Zombie)
+        self.assertIsInstance(self.monsters.get_monster("ZOMBIE", self.state, self.output_builder, "zombie_1"), Zombie)
 
     def test_get_monster_wrong(self) -> None:
-        self.assertIsNone(self.monsters.get_monster("human", "human_1"))
+        self.assertIsNone(self.monsters.get_monster("human", self.state, self.output_builder, "human_1"))
 
 
 class TestMonster(unittest.TestCase):
     """Test the Monster class and subclasses"""
     def setUp(self) -> None:
         self.monsters = MonsterCollection()
-
+        self.output_builder = OutputBuilder()
+        self.state = State(self.output_builder)
+        
     def test_monster_malformed(self) -> None:
         bad_monster1 = {
             "id": "cat",
@@ -62,9 +68,9 @@ class TestMonster(unittest.TestCase):
         }
         bad_monster2 = {"id": "cat", "name": "Cat", "type": "beast"}
         with self.assertRaises(AttributeError):
-            Monster(bad_monster1, unique_name="Cat 1")
+            Monster(bad_monster1, self.state, self.output_builder, unique_name="Cat 1")
         with self.assertRaises(AttributeError):
-            Monster(bad_monster2, unique_name="Cat 1")
+            Monster(bad_monster2, self.state, self.output_builder, unique_name="Cat 1")
 
 
 if __name__ == "__main__":
